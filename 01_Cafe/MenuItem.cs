d1727ed7da932e61f85d6ce8e2be89e76a4bd0c9
Andrew Foust <andfoust@gmail.com>
2020-10-01 10:33:14 -0400

    Add project files.

---
+++ 01_Cafe/MenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Cafe
+{
+    class MenuItem
+    {
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string MealDesc { get; set; }
+        public string Ingredients { get; set; }
+        public double Price { get; set; }
+
+        public MenuItem (int mealNumber, string mealName, string mealDesc, string ingredients, double price)
+        {
+            MealNumber = mealNumber;
+            mealName = mealName;
+            mealDesc = mealDesc;
+            Ingredients = ingredients;
+            Price = price;
+        }
+    }
+}
